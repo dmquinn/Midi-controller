99af3ae621ea3cf367eb4f7be6f6602b3b860069
dmquinn <danielmurphyberling@gmail.com>
2021-02-24 12:16:15 +0100

    added visual display, responsive to midi input

--- app.js
+++ app.js
@@ -35,11 +35,33 @@ var notes = {
 	32: "G5",
 	33: "G#5",
 	34: "A5",
+	56: "C1",
+	57: "E2",
+	58: "F2",
 };
+const container = document.getElementById("container");
+const colors = ["#1d1d1d", "#1d1d1d", "#1d1d1d", "#e65050", "#e63030"];
+const SQUARES = 300;
 
-const filter = new Tone.Filter().toDestination();
-const reverb = new Tone.Reverb(0.5).connect(filter);
-const synth = new Tone.PolySynth(Tone.FMSynth).connect(reverb);
+const reverb = new Tone.Reverb(10, 8).toDestination();
+const filter = new Tone.Filter().connect(reverb);
+const delay = new Tone.PingPongDelay().toDestination;
+var synth = new Tone.PolySynth(Tone.Synth, {
+	oscillator: {
+		type: "fatsawtooth",
+		count: 3,
+		spread: 30,
+	},
+	envelope: {
+		attack: 0.01,
+		decay: 0.1,
+		sustain: 0.5,
+		release: 0.4,
+		attackCurve: "exponential",
+	},
+}).connect(delay);
+
+const squaresound = [];
 var midi, data;
 if (navigator.requestMIDIAccess) {
 	navigator
@@ -54,44 +76,43 @@ if (navigator.requestMIDIAccess) {
 function onMIDISuccess(midiData) {
 	midi = midiData;
 	var allInputs = midi.inputs.values();
+	console.log(midi.outputs);
 
 	for (
 		var input = allInputs.next();
 		input && !input.done;
 		input = allInputs.next()
 	) {
+		if (!input) {
+			removeColor;
+		}
 		input.value.onmidimessage = playNote;
 	}
-}
-
-function playNote(messageData) {
-	console.log(messageData.data[1]);
-	i = messageData.data[1];
-	synth.triggerAttackRelease(Object.values([notes[i]]), 0.2);
-	console.log("ok", Object.keys(notes[i]));
 }
 
 function onMIDIFailure() {
 	console.warn("Not recognising MIDI controller");
 }
 
-//////////HOVERBOARD/////////////
-
-const container = document.getElementById("container");
-const colors = ["#e9e9e9", "#f1bfbf", "#e99393", "#e65050", "#e63030"];
-const SQUARES = 19;
-
 for (let i = 0; i < SQUARES; i++) {
-	i = messageData.data[1];
-	console.log("sq", i);
 	const square = document.createElement("div");
 	square.classList.add("square");
-
-	square.addEventListener("mouseover", () => setColor(square));
-
-	square.addEventListener("mouseout", () => removeColor(square));
-
 	container.appendChild(square);
+	squaresound.push(square);
+	console.log("squaresound", squaresound);
+}
+function playNote(messageData) {
+	console.log(messageData.data[1]);
+	i = messageData.data[1];
+
+	synth.triggerAttackRelease(Object.values([notes[i]]), 0.1);
+
+	// squaresound;
+
+	console.log("ok", i);
+	for (i = 0; i < squaresound.length; i++) {
+		setColor(squaresound[i]);
+	}
 }
 
 function setColor(element) {
@@ -107,4 +128,16 @@ function removeColor(element) {
 
 function getRandomColor() {
 	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function onDeviceInput({ input, value }) {
+	if (input === 82) inst.changeSynth(value);
+	else if (input === 2) inst.handleVolume(value);
+	else if (input === 14) inst.handleFilter(value);
+	else console.log("onDeviceInput!", input, value);
+}
+
+function changeSynth() {
+	synth.oscillator.type = "Sine";
+	console.log("changed");
 }
